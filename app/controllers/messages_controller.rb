6f03d2669ddc031568c4165bb7101af25b3a9dc2
nok2015 <standbyme20@hotmail.co.jp>
2015-09-20 00:21:57 +0000

    added edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit , :update]
+  
   def index
     # Messageをすべて取得する
     @messages = Message.all
     @message = Message.new
   end
+  
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -16,8 +19,26 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
+  
   def message_params
     params.require(:message).permit(:name, :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
